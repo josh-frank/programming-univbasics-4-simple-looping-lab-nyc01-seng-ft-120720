65376f78f64664c1f45aac2c394978c7b99fa5cc
Joshua Frank <eurosewusa@gmail.com>
2020-10-23 12:28:59 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -14,7 +14,7 @@ end
 
 def output_array( array )
   counter = 0
-  while counter[ array ] do
+  while counter < array.length do
     puts array[ counter ]
     counter += 1
   end
@@ -23,7 +23,7 @@ end
 def return_string_array( array )
   counter = 0
   while counter[ array ] do
-    array[ counter ] = array[ counter ].to_s
+    array[ counter ].to_s
     counter += 1
   end
   array
